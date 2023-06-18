fb88398788000702f7f32358670f33336e83e947
Dylan Weijgertze <dylanwprivate@gmail.com>
2023-06-18 23:34:44 +0200

    remapped accepting copilot suggestions and renamed neotree file

---
+++ .config/nvim/lua/custom/plugins/copilot.lua
@@ -0,0 +1,7 @@
+-- Remap <C-J> to accept the suggestion
+vim.g.copilot_no_tab_map = true
+vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+
+return {
+  "github/copilot.vim",
+}
